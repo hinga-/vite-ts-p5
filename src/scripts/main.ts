6dccd8e6638debcbe8c9a221dd0a2206a6503f55
hinga <higa0208@gmail.com>
2021-09-23 00:48:02 +0900

    :dizzy:

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,12 +3,16 @@ import p5 from 'p5'
 
 const sketch = (p5: p5) => {
   p5.setup = (): void => {
-    p5.createCanvas(400, 400)
+    p5.createCanvas(p5.windowWidth, p5.windowHeight)
   }
 
   p5.draw = (): void => {
     p5.background(500)
     p5.ellipse(50, 50, 80, 80)
+  }
+
+  p5.windowResized = (): void => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
   }
 }
 
